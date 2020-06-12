d31365ccfe98a0f3ffb6d42a6517ad3815f1ea59
cackharot <cackharot@gmail.com>
2020-06-12 17:10:56 -0400

    update setup config

--- setup.py
+++ setup.py
@@ -7,11 +7,22 @@ with open("README.md", "r") as fh:
 
 setup(
     name='asb-cli-explorer',
-    description="Small cli tool send and peek messages from Azure service bus",
+    description="Explore Azure service bus from command line",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='0.1.3',
-    packages=find_packages(),
+    version='0.1.4',
+    packages=find_packages(exclude=['tests']),
+    url='https://github.com/cackharot/asb-cli-explorer',
+    test_suite='tests',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords='service bus, servicebus, azure, azure service bus, messaging, amqp',
+    platforms='Python3',
     include_package_data=True,
     python_requires=">=3.7.4",
     install_requires=[
