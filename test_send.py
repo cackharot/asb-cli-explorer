fc3ec3440e9a6f9c66cd8cd710c94fcb21c6d142
cackharot <cackharot@gmail.com>
2020-06-12 17:28:37 -0400

    setup makefile, tests

---
+++ test_send.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock  # noqa
+
+
+def test_simple():
+    pass
